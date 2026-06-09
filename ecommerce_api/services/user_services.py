e0dc02a76326caf3fc2de7efe7731254312969c5
DaGitFella <davibezerra123457@gmail.com>
2026-06-09 19:25:13 -0300

    feat: creates user service for centralized user repo logic

---
+++ ecommerce_api/services/user_services.py
@@ -0,0 +1,15 @@
+from ecommerce_api.core.exceptions import ConflictError
+from ecommerce_api.models.users import User
+from ecommerce_api.repositories.user_repo import UserRepository
+from ecommerce_api.schemas.user_schema import UserCreate
+
+
+class UserService:
+    def __init__(self, repo: UserRepository) -> None:
+        self.repo = repo
+
+    def register(self, data: UserCreate) -> User:
+        if self.repo.email_exists(data.email):
+            raise ConflictError(f'Email {data.email} already taken.')
+        hashed_password = data.password
+        return self.repo.create_user(data, hashed_password)
