43a6a5c08bda5f2ad0b4b5e8e3aee6c5efaf096c
DaGitFella <davibezerra123457@gmail.com>
2026-06-14 10:26:53 -0300

    feat(database): creates conventions for contraint names and other special column names due to sqlite's limitations

--- ecommerce_api/infrastructure/database.py
+++ ecommerce_api/infrastructure/database.py
@@ -1,10 +1,20 @@
-from sqlalchemy import create_engine
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import Session, registry
 
 from ecommerce_api.core.settings import Settings
 
-table_registry = registry()
 engine = create_engine(Settings().DATABASE_URL, echo=True)
+
+convention = {
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(constraint_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s',
+}
+
+metadata_with_convention = MetaData(naming_convention=convention)
+table_registry = registry(metadata=metadata_with_convention)
 
 
 def get_db_session():
