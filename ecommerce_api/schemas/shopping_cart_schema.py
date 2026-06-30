54d5b0ad0fbda634cfcbc65a387c3ec478ee0130
DaGitFella <davibezerra123457@gmail.com>
2026-06-30 13:59:52 -0300

    fix: update shopping cart schema to correct optional field syntax error

--- ecommerce_api/schemas/shopping_cart_schema.py
+++ ecommerce_api/schemas/shopping_cart_schema.py
@@ -4,22 +4,21 @@ from pydantic import BaseModel
 
 
 class ShoppingCartCreate(BaseModel):
-    id: int
     shipping_type: str
-    shipping_cost: Optional[float] = float | None
+    shipping_cost: Optional[float] = None
 
 
 class ShoppingCartPublic(BaseModel):
     id: int
     user_id: int
     shipping_type: str
-    shipping_cost: Optional[float] = float | None
+    shipping_cost: Optional[float] = None
 
 
 class ShoppingCartUpdate(BaseModel):
     id: int
     shipping_type: str
-    shipping_cost: Optional[float] = float | None
+    shipping_cost: Optional[float] = None
 
 
 class ShoppingCartList(BaseModel):
