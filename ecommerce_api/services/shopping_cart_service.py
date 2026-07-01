259f3e76339c83e08aec48d965689bc82bc70127
davi <davibezerra123457@gmail.com>
2026-07-01 11:15:48 -0300

    feat(services): renome order service to shopping__cart_service. Implement shopping cart service and modifies user service due to shopping cart and user interdepence.

---
+++ ecommerce_api/services/shopping_cart_service.py
@@ -0,0 +1,34 @@
+import typing
+
+from ecommerce_api.core.constants import ShippingTypes
+from ecommerce_api.core.exceptions import MethodNotAllowedError
+from ecommerce_api.models import ShoppingCart, User
+from ecommerce_api.repositories.shopping_cart_repo import ShoppingCartRepository
+from ecommerce_api.schemas.shopping_cart_schema import (
+    ShoppingCartUpdate,
+)
+
+# this is provisory until current user is implemented,
+# then the user_id will be gotten from the current user
+
+
+class ShoppingCartService:
+    def __init__(self, repo: ShoppingCartRepository) -> None:
+        self.repo = repo
+
+    def create_default_shopping_cart(self, user: User) -> ShoppingCart:
+        # User must be gotten from current user whe it's implemented
+        return self.repo.create(
+            shipping_type=ShippingTypes.DELIVERY, user_id=user.id, user=user
+        )
+
+    def update_shopping_cart(self, id: int, data: ShoppingCartUpdate) -> ShoppingCart:
+        return self.repo.update(id=id, **data.model_dump())
+
+    @typing.override
+    def delete(self, id: int) -> None:
+        raise MethodNotAllowedError(
+            message='Deleting shopping cart is not allowed',
+            detail='Deleting shopping cart is not allowed',
+            allow=['GET', 'POST', 'PUT'],
+        )
--- ecommerce_api/services/user_services.py
+++ ecommerce_api/services/user_services.py
@@ -2,17 +2,25 @@ from ecommerce_api.core.exceptions import ConflictError
 from ecommerce_api.models.users import User
 from ecommerce_api.repositories.user_repo import UserRepository
 from ecommerce_api.schemas.user_schema import UserCreate, UserUpdate
+from ecommerce_api.services.shopping_cart_service import ShoppingCartService
 
 
 class UserService:
-    def __init__(self, repo: UserRepository) -> None:
-        self.repo = repo
+    def __init__(
+        self, user_repo: UserRepository, shopping_cart_service: ShoppingCartService
+    ) -> None:
+        self.repo = user_repo
+        self.shopping_cart_service = shopping_cart_service
 
     def register(self, data: UserCreate) -> User:
         if self.repo.email_exists(data.email):
             raise ConflictError(f'Email {data.email} already taken.')
         # do hashing here
-        return self.repo.create_user(data)
+        user = self.repo.create_user(data)
+        self.shopping_cart_service.create_default_shopping_cart(
+            user=user
+        )  # create default shopping cart for the user
+        return user
 
     def deactivate(self, id: int) -> User:
         return self.repo.update(id=id, is_active=False)
