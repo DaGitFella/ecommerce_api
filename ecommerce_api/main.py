bfa0833c3096e79498aa5578f11093ce24acba2f
davi <davibezerra123457@gmail.com>
2026-07-03 11:51:17 -0300

    feat: add event registration to app life cycle

--- ecommerce_api/main.py
+++ ecommerce_api/main.py
@@ -1,8 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
+from ecommerce_api.core.bootstrap import register_event_handlers
+from ecommerce_api.core.events import event_bus
 from ecommerce_api.core.exceptions import AppError
-from ecommerce_api.domain.users import routes
+from ecommerce_api.domains.shopping_carts.handlers import CartEventHandlers
+from ecommerce_api.domains.shopping_carts.service import ShoppingCartService
+from ecommerce_api.domains.users import routes
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    cart_handlers = CartEventHandlers(cart_service=ShoppingCartService())
+    register_event_handlers(event_bus, cart_handlers)
+    yield
 
 
 def create_app() -> FastAPI:
@@ -18,16 +31,18 @@ def create_app() -> FastAPI:
             },
         )
 
+    # config
+    app.title = 'ecommerce api'
+    app.version = '0.1'
+    app.description = 'trying to build an ecommerce api from scratch'
+
+    # routes
+    app.include_router(routes.router)
+
     return app
 
 
 app = create_app()
-
-app.title = 'ecommerce api'
-app.version = '0.1'
-app.description = 'trying to build an ecommerce api from scratch'
-
-app.include_router(routes.router)
 
 
 @app.get('/', status_code=200)
