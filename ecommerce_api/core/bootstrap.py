fe96a886bca74cc4c78e28fb0841468cf08f7164
davi <davibezerra123457@gmail.com>
2026-07-03 11:50:49 -0300

    feat: add event registration

---
+++ ecommerce_api/core/bootstrap.py
@@ -0,0 +1,9 @@
+from ecommerce_api.core.events.bus import EventBus
+from ecommerce_api.domains.shopping_carts.handlers import CartEventHandlers
+from ecommerce_api.domains.users.events import UserRegistered
+
+
+def register_event_handlers(
+    event_bus: EventBus, cart_handlers: CartEventHandlers
+) -> None:
+    event_bus.subscribe(UserRegistered, cart_handlers.on_user_registered)
