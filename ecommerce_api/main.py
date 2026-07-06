53a1c11089550b3f4f6ffc0c880bacb6bdbbd36e
davi <davibezerra123457@gmail.com>
2026-07-06 10:27:32 -0300

    fix: properly register the lifespan for the app

--- ecommerce_api/main.py
+++ ecommerce_api/main.py
@@ -7,19 +7,23 @@ from ecommerce_api.core.bootstrap import register_event_handlers
 from ecommerce_api.core.events import event_bus
 from ecommerce_api.core.exceptions import AppError
 from ecommerce_api.domains.shopping_carts.handlers import CartEventHandlers
+from ecommerce_api.domains.shopping_carts.repository import ShoppingCartRepository
 from ecommerce_api.domains.shopping_carts.service import ShoppingCartService
 from ecommerce_api.domains.users import routes
+from ecommerce_api.infrastructure.database import get_db_session
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    cart_handlers = CartEventHandlers(cart_service=ShoppingCartService())
+    cart_handlers = CartEventHandlers(
+        cart_service=ShoppingCartService(ShoppingCartRepository(get_db_session))
+    )
     register_event_handlers(event_bus, cart_handlers)
     yield
 
 
 def create_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
 
     @app.exception_handler(AppError)
     async def app_error_handler(request: Request, exc: AppError) -> JSONResponse:
