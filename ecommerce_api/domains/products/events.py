30ec271339fd262bdef15062d71457212083807e
DaGitFella <davibezerra123457@gmail.com>
2026-08-05 09:45:19 -0300

    feat(events): add specifications to ProductCreated events

--- ecommerce_api/domains/products/events.py
+++ ecommerce_api/domains/products/events.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from ecommerce_api.core.events.base import DomainEvent
 from ecommerce_api.domains.categories.schema import CategoryList
+from ecommerce_api.domains.specifications.schema import SpecificationList
 
 from .models import Product
 
@@ -9,4 +10,5 @@ from .models import Product
 @dataclass(frozen=True, kw_only=True)
 class ProductCreated(DomainEvent):
     categories: CategoryList
+    specifications: SpecificationList
     product: Product
