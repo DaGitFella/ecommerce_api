a4c62e24da15c3145a32d450a4b406f78d0eef6a
DaGitFella <davibezerra123457@gmail.com>
2026-08-05 11:57:04 -0300

    feat-test(specifications): add tests for retrieving specifications by slug and ID, including not found scenarios

--- tests/unit/services/test_specifications_service.py
+++ tests/unit/services/test_specifications_service.py
@@ -1,5 +1,6 @@
 import pytest
 
+from ecommerce_api.core.exceptions import NotFoundError
 from ecommerce_api.domains.specifications.models import SpecificationKey
 from ecommerce_api.domains.specifications.schema import SpecificationSchema
 
@@ -10,10 +11,79 @@ async def test_create_specification_must_return_specification_instance(
 ):
     service = fake_specifications_service_with_specifications
 
-    new_specification_data = SpecificationSchema(name='Color', slug='color')
+    new_specification_data = SpecificationSchema(name='Marca', slug='brand')
 
     result = await service.get_or_create_specification(new_specification_data)
 
     assert isinstance(result, SpecificationKey)
+    assert result.name == 'Marca'
+    assert result.slug == 'brand'
+
+
+@pytest.mark.asyncio
+async def test_get_or_create_specification_should_return_existing_specification(
+    fake_specifications_service_with_specifications,
+):
+    service = fake_specifications_service_with_specifications
+
+    existing_specification_data = SpecificationSchema(name='Color', slug='color')
+
+    result = await service.get_or_create_specification(existing_specification_data)
+
+    assert isinstance(result, SpecificationKey)
     assert result.name == 'Color'
     assert result.slug == 'color'
+
+
+@pytest.mark.asyncio
+async def get_specification_by_slug_should_return_specification_instance(
+    fake_specifications_service_with_specifications,
+):
+    service = fake_specifications_service_with_specifications
+
+    slug = 'color'
+
+    result = await service.get_specification_by_slug(slug)
+
+    assert isinstance(result, SpecificationKey)
+    assert result.name == 'Color'
+    assert result.slug == 'color'
+
+
+@pytest.mark.asyncio
+async def get_specification_by_id_should_return_specification_instance(
+    fake_specifications_service_with_specifications,
+):
+    service = fake_specifications_service_with_specifications
+
+    specification = await service.get_specification_by_slug('color')
+
+    result = await service.get_specification_by_id(specification.id)
+
+    assert isinstance(result, SpecificationKey)
+    assert result.name == 'Color'
+    assert result.slug == 'color'
+
+
+@pytest.mark.asyncio
+async def get_specification_by_id_should_return_not_found_error(
+    fake_specifications_service_with_specifications,
+):
+    service = fake_specifications_service_with_specifications
+
+    non_existent_id = 999  # An ID that doesn't exist
+
+    with pytest.raises(NotFoundError):
+        await service.get_specification_by_id(non_existent_id)
+
+
+@pytest.mark.asyncio
+async def get_specification_by_slug_should_return_not_found_error(
+    fake_specifications_service_with_specifications,
+):
+    service = fake_specifications_service_with_specifications
+
+    non_existent_slug = 'non-existent'  # A slug that doesn't exist
+
+    with pytest.raises(NotFoundError):
+        await service.get_specification_by_slug(non_existent_slug)
